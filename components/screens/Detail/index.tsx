6bfd053e3bf1eabc474eae4e123c275a9a1e4c8c
DeoSx <Deosxz123@gmail.com>
2020-09-22 22:21:26 +0600

    feat(detail): add button

--- components/screens/Detail/index.tsx
+++ components/screens/Detail/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 
-const Detail = () => {
+const Detail = (props: any) => {
   return (
     <View>
       <Text>Detail screen</Text>
+      <Button
+        title="to Home"
+        color="#000"
+        onPress={() => props.navigation.push('Home')}
+      />
     </View>
   );
 };
